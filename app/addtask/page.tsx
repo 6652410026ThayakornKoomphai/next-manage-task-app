5f7f5a18fd858cca96de50a4ac02ec9d41013e05
6652410026ThayakornKoomphai <s6652410026@sau.ac.th>
2025-10-01 17:06:38 +0700

    v1

---
+++ app/addtask/page.tsx
@@ -0,0 +1,92 @@
+"use client"
+import Image from "next/image"
+import logo from "./../../assets/logo.png"
+import Link from "next/link"
+import { useState, useEffect} from "react"
+import { supabase } from "../../lib/supabaseClient"
+
+export default function Page() {
+  //สร้างตัวแปร state เพื่อผูกกับข้อมูลที่เกิดขึ้นที่หน้าจอ และบันทึกลงฐานข้อมูล
+  const [title, setTitle] = useState('');
+  const [detail, setDetail] = useState('');
+  const [is_completed, setIsCompleted] = useState<boolean>(false); //ค่าเริ่มต้นคือ ยังไม่เริ่ม (0)
+  const [image_file, setImageFile] = useState<File | null >(null);
+  const [preview_file, setPreviewFile] = useState<string | null>(null); //เก็บ URL ของรูปภาพที่เลือกมาแสดงที่หน้าจอ
+
+  function handleSelectImagePreview(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0] || null;
+
+    setImageFile(file);
+
+    if(file){
+    setPreviewFile(URL.createObjectURL(file as Blob));
+    }
+  }
+
+  async function handleUploadAndSave(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    alert('อัปโหลดรูปภาพและบันทึกข้อมูล');
+  }
+
+  return (
+    <div className="flex flex-col w-10/12 mx-auto">
+    <div className="flex flex-col items-center  mt-20 ">
+      <Image 
+        src={logo}
+        alt="logo"
+        width={100}
+        height={100}
+      />
+      <h1 className="text-xl font-bold mt-5">
+        Manage Task App
+      </h1>
+      <h1 className="text-md mt-2 mb-10 font-bold">
+        บันทึกงานของคุณ
+      </h1>
+    </div>
+    {/* ส่วนของการเพิ่มงาน */}
+    <div className="flex  flex-col border border-gray-500 mt-10 p-5 rounded-xl">
+    <h1 className="text-center text-xl font-bold ">เพิ่มงาน</h1>
+
+    <form onSubmit={handleUploadAndSave}>
+    <div className="flex flex-col mt-5">
+      <label className="text-lg font-bold">งานที่ต้องทำ</label>
+      <input type="text" className="border border-gray-400 rounded-lg p-2 mt-2"/>
+    </div>
+
+    <div className="flex flex-col mt-5">
+          <label className="text-lg font-bold">รายละเอียดงาน</label>
+      <textarea className="border border-gray-500 rounded-lg p-2 "></textarea>
+    </div>
+
+    <div className="flex flex-col mt-5">
+      <label className="text-lg font-bold">อัพโหลดรูปภาพ</label>
+      <input type="file" className="hidden" accept="image/* " onChange={handleSelectImagePreview}/>
+      <label htmlFor="fildInput" className=" bg-blue-500 rounded-lg p-2 text-white cursor-pointer w-30 text-center mt-5">เลือกรูป</label>
+      {preview_file && (
+        <div className="mt-3">
+          <Image src={preview_file} alt="preview image" width={100} height={100} />
+        </div>
+      )}
+    </div>
+
+    <div className="flex flex-col mt-5">
+      <label className="text-lg font-bold"> สถานะงาน</label>
+      <select className="border border-gray-500 rounded-lg p-2">
+        <option value="0">ยังไม่เริ่ม</option>
+        <option value="1">เสร็จสิ้น</option>
+      </select>
+    </div>
+
+    <div className="flex flex-col mt-5">
+      <button type="submit" className="bg-green-500 rounded-lg p-2 text-white">บันทึกงานเพิ่มเติม</button>
+    </div>
+      </form>
+        <div className="flex justify-center my-10">
+      <Link href="/" className="bg-blue-500 hover:bg-blue-700 transition-all duration-300 text-white font-bold py-3 px-4 rounded">กลับไปหน้าแรก</Link>
+    </div>
+
+    </div>
+    </div>
+  )
+}
---
+++ app/alltask/page.tsx
@@ -0,0 +1,110 @@
+"use client"
+
+import Image from "next/image"
+import logo from "./../../assets/logo.png"
+import Link from "next/link"
+import { useState, useEffect} from "react"
+import { supabase } from "../../lib/supabaseClient"
+
+//สร้างประเภทตัวแปรเพื่อเก็บข้อมูลที่ดึงมาจาก supabase
+type Task = {
+  id: number
+  title: string
+  detail: string
+  is_completed: boolean
+  image_url: string
+  created_at: string
+  update_at: string
+}
+
+export default function Page() {
+  //สร้างตัวแปร state เพื่อเก็บข้อมูล task ที่ดึงมาจาก supabase
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  //เพื่อ Page ถูกโหลด จะดึงข้อมูล task จาก supabase มาแสดงที่หน้า Page
+  useEffect(() => {
+    async function fetchTasks() {
+      const { data, error } = await supabase
+        .from('task_tb')
+        .select('id, title, detail, is_completed, image_url, created_at, update_at') // เลือกข้อมูลทั้งหมด
+        .order('created_at', { ascending: false }); // เรียงลำดับข้อมูลจาก วันที่เพิ่ม(create_at) มากไปน้อย
+
+        if(error) {
+          alert('มี ข้อผิดพลาดในการดึงข้อมูล กรุณาลองใหม่อีกครั้ง');
+          console.log(error.message);
+        return;
+        }
+
+        if(data){
+          setTasks(data as Task[]); //เก็บข้อมูลที่ดึงมาไว้ที่ตัวแปร state tasks
+        }
+        
+  }
+
+  fetchTasks();
+  }, []);
+
+
+  return (
+    <div className="flex flex-col w-10/12 mx-auto">
+    <div className="flex flex-col items-center  mt-20 ">
+      <Image 
+        src={logo}
+        alt="logo"
+        width={100}
+        height={100}
+      />
+      <h1 className="text-xl font-bold mt-5">
+        Manage Task App
+      </h1>
+      <h1 className="text-md mt-2 mb-10 font-bold">
+        บันทึกงานของคุณ
+      </h1>
+    </div>
+    <div className="flex  justify-end">
+    <Link href='/addtask' className="mt-10 bg-blue-500 hover:bg-blue-700 transition-all duration-300 text-white font-bold py-3 px-4 rounded">
+    เพิ่มงาน
+    </Link>
+    </div>
+    
+    {/* ส่วนแสดงข้อมูลที่ดึงมาเป็นแบบตาราง */}
+    <div className="mt-5">
+    <table className="table-auto min-w-full border border-black text-sm mt-10">
+      <thead className="bg-gray-200">
+      <tr>
+        <th className="border border-black p-2">รูป</th>
+        <th className="border border-black p-2">งานที่ต้องทำ</th>
+        <th className="border border-black p-2">รายละเอียด</th>
+        <th className="border border-black p-2">สถานะ</th>
+        <th className="border border-black p-2">วันที่เพิ่ม</th>
+        <th className="border border-black p-2">วันที่แก้ไข</th>
+        <th className="border border-black p-2">Action</th>
+      </tr>
+      </thead>
+      <tbody>
+        {/* วน Loop ตามจำนวนข้อมูลที่อยู่ใน state: tasks */}
+        {tasks.map((task) => (
+          <tr key= {task.id}>
+            <td className="border border-black p-2">
+              {task.image_url 
+              ? <Image src={task.image_url} alt="logo" width={50} height={50}/> 
+              : '-'}
+            </td>
+            <td className="border border-black p-2">{task.title}</td>
+            <td className="border border-black p-2">{task.detail}</td>
+            <td className={`border border-black p-2 ${task.is_completed ? 'text-green-600' : 'text-red-600'}`}>{task.is_completed ? 'สำเร็จ' : 'ยังไม่สำเร็จ'}</td>
+            <td className="border border-black p-2">{new Date(task.created_at).toLocaleDateString()}</td>
+            <td className="border border-black p-2">{new Date(task.update_at).toLocaleDateString()}</td>
+            <td className="border border-black p-2"><Link href="#">แก้ไข</Link>
+            <button>ลบ</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>
+    <div className="flex justify-center my-10">
+      <Link href="/" className="bg-blue-500 hover:bg-blue-700 transition-all duration-300 text-white font-bold py-3 px-4 rounded">กลับไปหน้าแรก</Link>
+    </div>
+    </div>
+  )
+}
